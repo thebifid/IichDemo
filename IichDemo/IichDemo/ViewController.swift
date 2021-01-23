c82edacf29dfed7ca0246070fbb55d5ff2c565f4
vasilymatveev <vasily.matveev@saritasa.com>
2021-01-23 10:09:31 +0700

    first commit

---
+++ IichDemo/IichDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IichDemo
+//
+//  Created by Vasiliy Matveev on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
---
+++ IichDemo/IichDemoTests/IichDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  IichDemoTests.swift
+//  IichDemoTests
+//
+//  Created by Vasiliy Matveev on 23.01.2021.
+//
+
+import XCTest
+@testable import IichDemo
+
+class IichDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
